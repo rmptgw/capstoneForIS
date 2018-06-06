7ca5c0c6744f14b7a8647f8ede098c6a2f91e37c
rmptgw <rmptgw@gmail.com>
2018-06-06 22:15:10 +0900

    면접 관련 js파일 면접일 지정화면에서 넘어가는 동작 관여도중 작동이 멈춤

--- FMS/WebContent/script/interview.js
+++ FMS/WebContent/script/interview.js
@@ -14,12 +14,72 @@ function freeState(freeState){
 }
 
 function sendInterviewMsg(){
-
-	var url = "Eu?e=appointment_interview_form&no="+document.schduleFrm.chk.value;
+	// 면접일 지정을 위한 메시지창을 팝업창으로 띄워주는 메소드
+	
+	// checks 변수를 선언한 후 부모화면에서 이름이 checks인 체크박스의 value를 받아온다.
+	var checks = document.getElementsByName('checks');
+	
+	var chk = "";
+	
+	for(i=0; i<checks.length; i++){
+		// checks가 체크가 되었는지 확인한다.
+		if(checks[i].checked === true){
+			
+			// 체크가 되었으면 chk에 해당 checks의 value를 '/'로 구분하여 저장한다.
+			chk += checks[i].value + "/";
+		}
+	}
+	
+	if(chk == ""){
+		// 선택된 체크박스가 없으면 실행이 선택된 프리랜서가 없다고 알림창이 뜬다.
+		alert("면접을 볼 프리랜서를 선택해 주세요");
+		return false;
+	}
+	/*
+	alert(chk);
+	*/
+	
+	// 펼쳐질 팝업창의 주소
+	var url = "Eu?e=appointment_interview_form&chk=" + chk;
+	
+	// 팝업창을 펼친다.
 	window.open(url, "_blank_1",
 	"toolbar=no, menubar=no, scrollbars=yes, resizable=no, width=600, height=300");
 	
 }
+
+function sendAppointmentMsg(){
+	/*체크해야 할 것들
+	date(면접일), time(면접시간), location(면접 장소), content(면접관련 알림말)*/
+	if(document.frm.date.value == null || document.frm.date.value==""){
+		alert("면접일을 지정해주세요");
+		return false;
+	}
+	if(document.frm.time.value == null || document.frm.time.value == ""){
+		alert("면접 시간을 지정해주세요");
+		return false;
+	}
+	if(document.frm.location.value == null || document.frm.location.value == ""){
+		alert("면접 장소를 적어주세요");
+		return false;
+	}
+	
+	var inputs = document.getElementByName("joinNum");
+	var no = "";
+	
+	for(i = 0; inputs.length; i++){
+		no += inputs[i].value + "/";
+	}
+	
+	alert(no);
+	
+	var url = "Eu?e=appointment_interview&no =" + no;
+	
+	location.href="url";
+	
+	self.close();
+}
+
 function sendResultMsg(){
 	if(document.resultInterviewFrm.freeState == null || 
 			document.resultInterviewFrm.freeState == ""){
